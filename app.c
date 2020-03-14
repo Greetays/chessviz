ad9869c97220157037a71d549d211a928e5fa4bc
Мустафин Ренат Илнурович <iv922s16@bwp09.csc.local>
2020-03-14 17:07:49 +0700

    Test

--- app.c
+++ app.c
@@ -2,7 +2,9 @@
 
 int main()
 {
-    char ch[9][9] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
+    int k2, k22, m1, m11;
+    char k1, t, k11, figure;
+    char ch[8][8] = {{'r', 'n', 'b', 'q', 'k', 'b', 'n', 'r'},
                      {'p', 'p', 'p', 'p', 'p', 'p', 'p', 'p'},
                      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
@@ -10,8 +12,36 @@ int main()
                      {' ', ' ', ' ', ' ', ' ', ' ', ' ', ' '},
                      {'P', 'P', 'P', 'P', 'P', 'P', 'P', 'P'},
                      {'R', 'N', 'B', 'Q', 'K', 'B', 'N', 'R'}};
-    for (int i = 0; i < 9; i++) {
+    for (int i = 0; i < 8; i++) {
         for (int j = 0; j < 8; j++) {
+            printf("%c ", ch[i][j]);
+        }
+        printf("\n");
+    }
+    printf("White's move. Enter the coordinates: ");
+    scanf("%d%d%d%d%d", k1, k2, t, k11, k22);
+    k1 = (k1 - 1) % 10;
+    k11 = (k11 - 1) % 10;
+    printf("%d, %d", k1, k11);
+    for (int i = 0; i < 8; i++){
+        for(int j = 0; j < 8; j++){
+            if (j == k1 && i == k2 - 1){
+                figure == ch[i][j];
+                ch[i][j] = ' ';
+            }
+        }
+    }
+    
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
+            if (j == k11 && i == k22){
+                ch[i][j] = figure;
+            }
+        }
+    }
+    
+    for (int i = 0; i < 8; i++){
+        for (int j = 0; j < 8; j++){
             printf("%c ", ch[i][j]);
         }
         printf("\n");
